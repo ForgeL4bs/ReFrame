cda8aecb806ff65eb88973b6bf34a1b55caa4126
73LIX <gouravydv.exe@gmail.com>
2025-04-29 02:31:12 +0530

    created subcommands and integrated resize

--- ReFrame/cli.py
+++ ReFrame/cli.py
@@ -2,10 +2,20 @@ import argparse
 from ReFrame.extract_frames import extract_frames
 from ReFrame.image_converter import convert_image
 from ReFrame.gif_creator import create_gif
+from ReFrame.image_resizer import resize_image_files
 
 def main():
     parser = argparse.ArgumentParser(
-        description="ReFrame-CLI. A great tool to boost your productivity in video and image manipulation tasks. Ideal for preparing image datasets for training machine learning models, including generative AI and diffusion models."
+        description=(
+            "ReFrame-CLI: ImageToolKit\n"
+            "Now supports frame extraction, image conversion, GIF creation, and image resizing.\n\n"
+            "Examples:\n"
+            "  Extract frames: reframe extractf -input video.mp4 -output ./frames -fps 1\n"
+            "  Convert images: reframe convert -input ./images -output ./converted -f png\n"
+            "  Create GIF: reframe gifc -input ./images -output ./output.gif -d 100\n"
+            "  Resize images: reframe resize -input ./images -output ./resized -wh 800 -ht 600\n"
+        ),
+        formatter_class=argparse.RawTextHelpFormatter
     )
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
@@ -23,7 +33,7 @@ def main():
                                 help="End time (in seconds) for frame extraction.")
 
     #subcommand: convert
-    convert_parser = subparsers.add_parser("convert", help="Convert images to different formats")
+    convert_parser = subparsers.add_parser("convert", help="Convert images to different formats(e.g., png, jpg, jpeg, heif, etc.)")
     convert_parser.add_argument("-input", "--input_path", required=True, help="Path to the image file or directory")
     convert_parser.add_argument("-output", "--output_dir", required=True, help="Directory to save the converted images")
     convert_parser.add_argument("-f", "--format", required=True, choices=["png", "jpg", "jpeg", "webp", "heic", "heif"],
@@ -31,10 +41,22 @@ def main():
 
     #subcommand: gif
     gif_parser = subparsers.add_parser("gifc", help="Create an animated GIF by stakcing up images")
-    gif_parser.add_argument("-input", "--input_dir", required=True, help="Path to the directory containing images")
-    gif_parser.add_argument("-output", "--output_path", required=True, help="Path to save the output GIF file must be specified with .gif format")
+    gif_parser.add_argument("-input", "--input_path", required=True, help="Path to the directory containing images")
+    gif_parser.add_argument("-output", "--output_dir", required=True, help="Path to save the output GIF file must be specified with .gif format")
     gif_parser.add_argument("-d", "--duration", type=int, default=100,
                              help="Duration of each frame in the GIF in milliseconds (default: 100ms)")
+    
+    #subcommand: resize
+    resize_parser = subparsers.add_parser("resize", help="Resize images with focal point control")
+    resize_parser.add_argument("-input", "--input_path", required=True, help="Path to the image file or directory")
+    resize_parser.add_argument("-output", "--output_dir", required=True, help="Directory to save the resized images")
+    resize_parser.add_argument("-wh", "--width", type=int, help="The width to which you need to resize the original image")
+    resize_parser.add_argument("-ht", "--height", type=int, help="The height to which you need to resize the original image")
+    resize_parser.add_argument("-ratio", "--ratio", type=str, help="The desired aspect ratio (e.g., '1:1', '1:2', '3:4')")
+    resize_parser.add_argument("-fp", "--focal_point", type=str, default="auto",
+                            help="The focal point for resizing (left, right, top, bottom, center, auto, default: auto)")
+    resize_parser.add_argument("-f", "--format", type=str, choices=["png", "jpg", "jpeg"], help="The desired output format (png, jpg, jpeg)")
+    resize_parser.add_argument("-multi", "--multiplier", type=float, help="The resizing multiplier (e.g., 2 for 2x)")
 
     #parse the arguments
     args = parser.parse_args()
@@ -57,10 +79,21 @@ def main():
         )
     elif args.command == "gifc":
         create_gif(
-            image_dir=args.input_dir,
-            output_path=args.output_path,
+            image_dir=args.input_path,
+            output_dir=args.output_dir,
             duration=args.duration,
         )
+    elif args.command == "resize":
+        resize_image_files(
+            image_path=args.input_path,
+            output_dir=args.output_dir,
+            width=args.width,
+            height=args.height,
+            ratio=args.ratio,
+            focal_point=args.focal_point,
+            output_format=args.format,
+            multiplier=args.multiplier,
+    )
     else:
         parser.print_help()
 
