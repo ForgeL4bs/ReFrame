4dc3fd7fed522e88bebf0479f2831f9270932044
73LIX <gouravydv.exe@gmail.com>
2025-04-29 02:36:45 +0530

    modified setup.py for the newer version ~ reframe-cli 1.1.0

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 
 setup(
     name="reframe-cli",
-    version="1.0.0",
-    description="ReFrame-CLI A great tool to boost your productivity in video and image manipulation tasks. Ideal for preparing image datasets for training machine learning models, including generative AI and diffusion models.",
-    long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    version="1.1.0",
+    description="ReFrame-CLI is a Python-based command-line tool to streamline your video and image manipulation tasks. Ideal for preparing image datasets for training machine learning models, including generative AI and diffusion models. Can handle videos of any length.",
+    long_description=open("readme.md").read() if os.path.exists("readme.md") else "",
     long_description_content_type="text/markdown",
     author="Gour4v",
     author_email="chatgai.note@gmail.com",
@@ -28,4 +28,9 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    project_urls={
+        "GitHub": "https://github.com/ForgeL4bs/ReFrame",
+        "Documentation": "https://github.com/ForgeL4bs/ReFrame#readme",
+        "Issue Tracker": "https://github.com/ForgeL4bs/ReFrame/issues",
+    },
 )
